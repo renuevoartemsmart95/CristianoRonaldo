d4630c9863a4a2e617da6aae17bd5d7c01d5693d
renuevoartemsmart95 <71007758+renuevoartemsmart95@users.noreply.github.com>
2021-02-13 19:43:01 +0100

    Buenas,querido portugués Dos Santos Aveiro

---
+++ Cristiano Ronaldo/src/cristiano/ronaldo/CristianoRonaldo2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cristiano.ronaldo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class CristianoRonaldo2 {
+    String personalidad;
+    int goles = 750;
+    int asistencias = 268;
+}
